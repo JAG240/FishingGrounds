3b8b213c0cea3ca4af5a43c9bb67f69036735830
Jacob Gonos <jacobgonos@gmail.com>
2022-01-06 21:28:16 -0500

    First Commit
    This has a couple very basic setups. No actual finished code or work. This menu screen and test scene will be used temporarily for local multiplayer testing. Only a scene loader buttons, scene loader logic, and place holder player prefab.

---
+++ Scripts/Managers/SceneLoader.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+using Unity.Netcode;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    private void Awake()
+    {
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+    //When starting as host only load scene
+    public void StartAsHost()
+    {
+        SceneManager.LoadScene("Fish Lab");
+    }
+
+    //If you are a client, connect as client and load the same scene as host 
+    public void StartAsClient()
+    {
+        NetworkManager.Singleton.StartClient();
+    }
+
+    //When a new scene is loaded, if you are the host start as host. 
+    private void OnLevelWasLoaded(int level)
+    {
+        if (!NetworkManager.Singleton.IsClient)
+            NetworkManager.Singleton.StartHost();
+    }
+}
